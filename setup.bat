b614306a56c39fc22a19ca8c94f5d01d1ab72a51
Osama Alhamdan <osama.alhamdan133@gmail.com>
2026-09-20 06:55:08 +0300

    chore: add fast setup script to inject atv command into user PATH

---
+++ setup.bat
@@ -0,0 +1,1 @@
+@echo off`ncopy "dist\atv.exe" "%LOCALAPPDATA%\Microsoft\WindowsApps\atv.exe"`necho.`necho Installation Complete! You can now type "atv" anywhere in CMD or Win+R to launch your TV OS!`npause
