67e1a87e40ed3bb2d823207313c1032bfbf27c44
Osama Alhamdan <osama.alhamdan133@gmail.com>
2026-09-20 07:02:01 +0300

    chore: add uninstall script to safely remove the atv command from the system PATH

---
+++ uninstall.bat
@@ -0,0 +1,1 @@
+@echo off`necho Uninstalling Abo7amdanTV OS...`ndel "%LOCALAPPDATA%\Microsoft\WindowsApps\atv.exe"`necho.`necho Uninstallation Complete. The "atv" command has been removed from your system.`npause
